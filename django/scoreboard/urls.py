2a44dae12f7ea538c44f29ffd0d519171502f68f
David Bourgault <dav.bour@gmail.com>
2017-12-17 17:04:46 -0500

    online scoreboard is now a single-page app

--- django/scoreboard/urls.py
+++ django/scoreboard/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<track>/', views.track_normal, name='track_normal'),
-    path('<track>/<mode>/', views.track_global, name='track_global'),
-    path('<track>/<mode>/<category>/', views.track_category, name='track_category'),
+    path('', views.scoreboard, name='scoreboard'),
+    path('json/<track>/<mode>/<category>/', views.scoreboard_json, name='json_scoreboard'),
     path('sync', views.sync, name='sync'),
 ]
--- django/scoreboard/views.py
+++ django/scoreboard/views.py
@@ -10,40 +10,24 @@ from .models import Time, Car
 from .utils import *
 
 # Create your views here.
-def index(request):
+def scoreboard(request):
     return render(request, 'scoreboard/index.html')
 
-
-def track_normal(request, track):
-    return redirect('track_category', track=track, mode='normal', category='global')
-
-
-def track_global(request, track, mode):
-    return redirect('track_category', track=track, mode=mode, category='global')
-
-
-def track_category(request, track, mode, category):
+def scoreboard_json(request, track, mode, category):
     track = track.replace('-',' ')
     times = Time.objects.filter(track=track, mode=mode).order_by('time')
 
     if category != 'global':
         times = times.filter(car__category=category)
-    
-    times = times[:10]
-    context = {
-        'times': times,
-        'track': track.capitalize(),
-        'mode' : mode.capitalize() if mode != 'reversemirror' else 'Reverse Mirror',
-        'category': category.capitalize() 
-    }
-    return render(request, 'scoreboard/score.html', context)
 
+    return render(request, 'scoreboard/score.json', {'times': times[:10]})
 
 @csrf_exempt
 def sync(request):
     if request.method == 'POST':
         new = add_times_from_json(request.body)
         return HttpResponse(str(new))
+
     if request.method == 'GET':
         payload = get_times_json()
         return HttpResponse(payload)
